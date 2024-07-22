1dbd435dc46c7c69a8172e972f70622d3990d967
Harsh <harshchamola@gmail.com>
2024-07-23 03:42:56 +0400

    final

--- server/index.js
+++ server/index.js
@@ -82,7 +82,7 @@ app.get('/video/:id', async (req, res) => {
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
 });
 
 // Running the server on port 8000
